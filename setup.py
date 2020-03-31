b11c3e0586e60c2be9e92c62c8e209ba9afbce7d
Justin Terry <justinkterry@gmail.com>
2020-03-31 16:29:50 -0400

    fix name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rlcard",
+    name="magent",
     version="0.1.0",
     author="PettingZoo Team",
     author_email="justinkterry@gmail.com",
