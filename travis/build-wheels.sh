ac89f023b07122ac323cfc65e7b87056a4a14b70
marioj <mario.jayakumar@gmail.com>
2020-06-26 18:59:49 -0400

    fixed bash spacing

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -2,7 +2,7 @@
 set -e -u -x
 
 for PYBIN in /opt/python/*/bin; do
-    if [["$PYBIN" != *"27"*]]; then
+    if [[ "$PYBIN" != *"27"* ]]; then
         "${PYBIN}/pip" install -r /io/requirements.txt
         "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
     fi
