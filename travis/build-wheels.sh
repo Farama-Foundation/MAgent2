b0c3f5c39fb30b2d50c9cee29da0400a6b5d631a
marioj <mario.jayakumar@gmail.com>
2020-06-26 18:52:29 -0400

    docker build proper python version regex

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -2,7 +2,7 @@
 set -e -u -x
 
 for PYBIN in /opt/python/*/bin; do
-    if ["${PYBIN}" -ne "cp27-cp27m"]; then
+    if ["$PYBIN" != *"27"*]; then
         "${PYBIN}/pip" install -r /io/requirements.txt
         "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
     fi
