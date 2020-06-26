67db4ff0b27937cecb0001e43c16d808af823c7b
marioj <mario.jayakumar@gmail.com>
2020-06-26 16:53:31 -0400

    build wheel ignore python2.7

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -2,6 +2,8 @@
 set -e -u -x
 
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/requirements.txt
-    "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
+    if ["${PYBIN}" -ne "cp27-cp27m"]; then
+        "${PYBIN}/pip" install -r /io/requirements.txt
+        "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
+    fi
 done
