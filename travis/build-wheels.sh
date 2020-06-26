c6282d4e3d244562d9b625b91bc549317d6e5aca
marioj <mario.jayakumar@gmail.com>
2020-06-26 15:56:29 -0400

    fixed requirements for docker

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -2,6 +2,6 @@
 set -e -u -x
 
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
 done
