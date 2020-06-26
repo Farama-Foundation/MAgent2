6646cbd88cae7ecf910c251932649ac80452b10c
marioj <mario.jayakumar@gmail.com>
2020-06-26 14:25:28 -0400

    docker explicitly state python version

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e -u -x
 
-pip install -r /io/dev-requirements.txt
-pip wheel /io/ --no-deps 
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
+done
