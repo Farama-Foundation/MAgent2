a4e62261ee02f659e35f32587aca186d7bb12326
marioj <mario.jayakumar@gmail.com>
2020-06-26 20:50:46 -0400

    hide output, only use python 36, 37, or 38 for build

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -3,16 +3,16 @@ set -e -u -x
 
 yum install -y wget
 
-wget https://github.com/Kitware/CMake/releases/download/v3.15.2/cmake-3.15.2.tar.gz
+wget https://github.com/Kitware/CMake/releases/download/v3.15.2/cmake-3.15.2.tar.gz > /dev/null
 tar -zxvf cmake-3.15.2.tar.gz > /dev/null
 cd cmake-3.15.2
-./bootstrap
+./bootstrap > /dev/null
 make -j$(nproc)
 make install
 cd ..
 
 for PYBIN in /opt/python/*/bin; do
-    if [[ ( "$PYBIN" != *"27"* ) && ( "$PYBIN" != *"35"* ) ]]; then
+    if [[ ( "$PYBIN" == *"36"* ) || ( "$PYBIN" != *"37"* ) || ( "$PYBIN" != *"38"* ) ]]; then
         "${PYBIN}/pip" install -r /io/requirements.txt
         "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
     fi
