d3ff90149fcdacc51995610af465f5d28d1f8dc4
marioj <mario.jayakumar@gmail.com>
2020-06-26 21:03:08 -0400

    test faster cmake build step

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -3,16 +3,16 @@ set -e -u -x
 
 yum install -y wget
 
-wget https://github.com/Kitware/CMake/releases/download/v3.15.2/cmake-3.15.2.tar.gz > /dev/null
-tar -zxvf cmake-3.15.2.tar.gz > /dev/null
-cd cmake-3.15.2
-./bootstrap > /dev/null
-make -j$(nproc)
-make install
+CMAKE_DIR="/cmk"
+mkdir ${CMAKE_DIR} && cd ${CMAKE_DIR}
+wget --no-check-certificate https://github.com/Kitware/CMake/releases/download/v3.17.3/cmake-3.17.3-Linux-x86_64.tar.gz
+tar -xvf cmake-3.17.3-Linux-x86_64.tar.gz > /dev/null
+mv cmake-3.17.3-Linux-x86_64 cmake-install
+PATH=${CMAKE_DIR}/cmake-install:${CMAKE_DIR}/cmake-install/bin:$PATH
 cd ..
 
 for PYBIN in /opt/python/*/bin; do
-    if [[ ( "$PYBIN" == *"36"* ) || ( "$PYBIN" != *"37"* ) || ( "$PYBIN" != *"38"* ) ]]; then
+    if [[ ( "$PYBIN" == *"36"* ) || ( "$PYBIN" == *"37"* ) || ( "$PYBIN" == *"38"* ) ]]; then
         "${PYBIN}/pip" install -r /io/requirements.txt
         "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
     fi
