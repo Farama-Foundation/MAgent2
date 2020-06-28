dab3a1ab7931ef39f272659fd0765dd6f69feeba
marioj <mario.jayakumar@gmail.com>
2020-06-28 11:15:20 -0400

    bundle built wheels with audit

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e -u -x
 
+
+function repair_wheel {
+    wheel="$1"
+    if ! auditwheel show "$wheel"; then
+        echo "Skipping non-platform wheel $wheel"
+    else
+        auditwheel repair "$wheel" --plat "$PLAT" -w /io/wheelhouse/
+    fi
+}
+
+
 yum install -y wget
 
 CMAKE_DIR="/cmk"
@@ -16,4 +27,9 @@ for PYBIN in /opt/python/*/bin; do
         "${PYBIN}/pip" install -r /io/requirements.txt
         "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
     fi
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    repair_wheel "$whl"
 done
