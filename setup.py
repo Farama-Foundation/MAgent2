7a80d529ef1466bb03657bfd41f47dd12fcd1b31
dsctt <dascott321@gmail.com>
2022-03-04 01:48:37 -0500

    setup.py python versions 3.7-10

--- setup.py
+++ setup.py
@@ -99,11 +99,12 @@ setuptools.setup(
         'numpy>=1.18.0',
         'pygame>=2.0.0.dev10'
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     classifiers=[
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
