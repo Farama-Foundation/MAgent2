4966806d961f7627422f4de1136483af4dfa000a
StringTheory <mark.m.towers@gmail.com>
2022-08-31 18:19:01 +0100

    Remove local imports

--- examples/models/mx_model/__init__.py
+++ examples/models/mx_model/__init__.py
@@ -1,2 +1,2 @@
-from .a2c import AdvantageActorCritic
-from .dqn import DeepQNetwork
+from examples.models.mx_model.a2c import AdvantageActorCritic
+from examples.models.mx_model.dqn import DeepQNetwork
