014489fcaaf2da7d54e00ca72f08eb1d4c668dcc
dsctt <dascott321@gmail.com>
2022-10-24 16:46:37 -0400

    Remove old examples

--- examples/renderer/server/__init__.py
+++
@@ -1,5 +0,0 @@
-from examples.renderer.server.arrange_server import ArrangeServer
-from examples.renderer.server.base_server import BaseServer
-from examples.renderer.server.battle_server import BattleServer
-from examples.renderer.server.random_server import RandomServer
-from examples.renderer.server.sample_server import SampleServer
