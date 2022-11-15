41370006d57c925433f70065b86bc115912c20c3
Feng <59535397+SiddarGu@users.noreply.github.com>
2022-11-14 19:44:59 -0500

    Update gridworld.py

--- magent2/gridworld.py
+++ magent2/gridworld.py
@@ -376,12 +376,9 @@ class GridWorld(Environment):
             method (str): Can be 'random' or 'custom'. If method is 'random', then kwargs["n"] is an int.
                 If method is 'custom', then kwargs["pos"] is a list of coordination
 
-        .. code-block:: python
-            # add 1000 walls randomly
-            >>> env.add_walls(method="random", n=1000)
-            
-            # add 3 walls to (1,2), (4,5) and (9, 8) in map
-            >>> env.add_walls(method="custom", pos=[(1,2), (4,5), (9,8)])
+        Example:
+            >>> env.add_walls(method="random", n=1000)                    # add 1000 walls randomly
+            >>> env.add_walls(method="custom", pos=[(1,2), (4,5), (9,8)]) # add 3 walls to (1,2), (4,5) and (9, 8) in map
         """
         # handle = -1 for walls
         kwargs["dir"] = 0
