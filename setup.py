9fee3e26443bd9645c2910831912d400192804a9
snow-fox <taijunjet@hotmail.com>
2023-01-06 17:37:50 +0000

    add back long description

--- setup.py
+++ setup.py
@@ -111,9 +111,25 @@ class CMakeBuild(build_ext):
             # )
 
 
+def get_description():
+    """Gets the description from the readme."""
+    with open("README.md") as fh:
+        long_description = ""
+        header_count = 0
+        for line in fh:
+            if line.startswith("##"):
+                header_count += 1
+            if header_count < 2:
+                long_description += line
+            else:
+                break
+    return long_description
+
+
 setuptools.setup(
     name="magent2",
     version=get_version(),
+    long_description=get_description(),
     ext_modules=[CMakeExtension("magent2.libmagent", ".", [])],
     cmdclass={"build_ext": CMakeBuild},
 )
